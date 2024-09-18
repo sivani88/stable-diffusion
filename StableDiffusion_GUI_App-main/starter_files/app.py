0e7c743c1ba9a067fc7a60d11ebf412dcfb68e3d
sivani88 <84349314+sivani88@users.noreply.github.com>
2024-09-18 11:17:52 +0200

    7 commit

--- StableDiffusion_GUI_App-main/starter_files/app.py
+++ StableDiffusion_GUI_App-main/starter_files/app.py
@@ -7,27 +7,51 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 pipeline = StableDiffusionPipeline.from_pretrained("./stable-diffusion-v1-4")
+pipeline.enable_freeu(b1= 1.3, b2= 1.4, s1= 0.9, s2= 0.2)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    # home page
+    
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["/static/images/placeholder_image.png" for i in range(3)]
+    )
 
-@app.route('/prompt', methods=['POST'])
+@app.route('/prompt', methods=['POST', 'GET'])
 def prompt():
-    prompt_text = request.form.get('prompt_input', '').strip()
-    if not prompt_text:
-        return render_template("index.html", error="No prompt provided!")
+    # generate images from user prompt
+    print("user prompt received:", request.form['prompt_input'])
+    
+    for i in range(3):
+        image = pipeline(request.form['prompt_input']).images[0]
+        image.save("static/images/demo_img" + str(i) + ".png")
 
-    print("User prompt received:", prompt_text)
-    try:
-        image = pipeline(prompt_text).images[0]
-        image_path = "static/images/demo_img0.png"
-        image.save(image_path)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return render_template("index.html", error=str(e))
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["static/images/demo_img" + str(i) + ".png" for i in range(3)]
+    )
 
-    return render_template("index.html", prompt_image=f"/{image_path}")
+@app.route('/supersample', methods=['POST', 'GET'])
+def supersample():
+    # enlarge and save prompt image in high quality
+    print("save button", request.form['save_btn'], "was clicked!")
+
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["/static/images/placeholder_image.png" for i in range(3)]
+    )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    # run application
+    app.run(
+        host = '0.0.0.0', 
+        port = 8000, 
+        debug = True
+    )   
